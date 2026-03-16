8378859f5b018f3e8a2cf18de46d4b919f48e4f8
riceball <42161586+riceball-tw@users.noreply.github.com>
2026-03-16 23:04:52 +0800

    feat: Add starter level

--- level.go
+++ level.go
@@ -9,10 +9,10 @@ type Level struct {
 }
 
 var level1 = Level{
-	Title:       "Level 1",
+	Title:       "Level 1 - Walk",
 	Description: "use h, j, k, l to move, u to undo, q to quit",
 	Grid: [][]Tile{
-		{w, w, doorToLevel2, w, w},
+		{w, w, d2, w, w},
 		{w, e, e, e, w},
 		{w, e, e, e, w},
 		{w, e, e, e, w},
@@ -21,19 +21,54 @@ var level1 = Level{
 	StartX: 2,
 	StartY: 2,
 }
-var doorToLevel2 = newDoor(level2)
-
+var d2 = newDoor(level2)
 
 var level2 = Level{
-	Title:       "Level 2",
-	Description: "It's a bit empty here.",
+	Title:       "Level 2 - Push",
+	Description: "Push box +1 count",
 	Grid: [][]Tile{
-		{e},
+		{w, w, w, w, w, w, w, w},
+		{w, e, e, e, e, e, e, w},
+		{w, e, e, e, newBox(), e, d3, w},
+		{w, e, e, e, e, e, e, w},
+		{w, w, w, w, w, w, w, w},
 
 	},
-	StartX: 0,
-	StartY: 0,
+	StartX: 2,
+	StartY: 2,
 }
+var d3 = newLock(1, newDoor(level3))
+
+var level3 = Level{
+	Title:       "Level 3 - Pull",
+	Description: "Hold shift to pull box, -1 count, minium 0",
+	Grid: [][]Tile{
+		{w, w, w, w, w, w, w, w, w, w},
+		{w, e, e, e, e, e, e, e, e, w},
+		{w, e, e, e, d4, e, e, newBox(), e, w},
+		{w, e, e, e, e, e, e, e, e, w},
+		{w, w, w, w, w, w, w, w, w, w},
+
+	},
+	StartX: 2,
+	StartY: 2,
+}
+var d4 = newLock(1, newDoor(level4))
+
+
+var level4 = Level{
+	Title:       "Level 4 - Combine",
+	Description: "Combine 2 box",
+	Grid: [][]Tile{
+		{w, w, w, w, w, w, w, w},
+		{w, e, newBox(), e, newBox(), e, d5, w},
+		{w, e, e, e, e, e, e, w},
+		{w, w, w, w, w, w, w, w},
+	},
+	StartX: 1,
+	StartY: 1,
+}
+var d5 = newLock(4, newDoor(winLevel))
 
 var winLevel = Level{
 	Title:       "Victory",
